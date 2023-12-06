1e898f9c069bda0dd02a209611afee0e076eac2e
경민 <min99819@pusan.ac.kr>
2023-12-06 14:24:02 +0900

    create post table

--- public/javascripts/check_dup.js
+++ public/javascripts/check_dup.js
@@ -1,6 +1,6 @@
 // 중복확인 창 열기
 
 function open_dup_window(){
-    gsWin = window.open("register/dup_chk", "_blank", "popup=1");
+    gsWin = window.open("register/dup_chk", "_blank", "width=600,height=400");
     value_id = gsWin.document.getElementById("dup_id")
     }
